e0eac202fd031566e3a76944f8b42aa0ad529da5
Kenji Fujiwara <niyaton@gmail.com>
2014-03-06 13:55:23 +0900

    refactored

--- kakimoti/kakimoti.py
+++ kakimoti/kakimoti.py
@@ -12,25 +12,34 @@ RAINFALL = ('Rainfall', 'mm/h', 1)
 #WINDIR = ('WinDir', 'mm/h')
 #WINDSPEED = 'WindSpeed'
 
-output_data = [TEMPERATURE, HUMIDITY, PRESSURE, RAINFALL]
-
-data = json.loads(urlopen(LIVE_E_NAIST_URL).read().decode('utf8'))
-print(json.dumps(data, sort_keys=True, indent=4, separators=(',', ':')))
-
-time = dateutil.parser.parse(data[TEMPERATURE[0]]['time'])
-
-#temperature = data[TEMPERATURE]
-
-def get_live_e_data_str():
+def get_live_e_data_str(livee_json, output_data):
     for key, unit, ndigits in output_data:
-        value = round(float(data[key]['value']), ndigits)
+        value = round(float(livee_json[key]['value']), ndigits)
         format_str = '{0:.' + str(ndigits) + 'f}({1})'
         yield format_str.format(value, unit)
 
-tweet = ','.join(get_live_e_data_str())
-tweet += ' 【' + time.strftime("%Y-%m-%d %H:%M:%S") + '】'
+def get_tweet():
+    output_data = [TEMPERATURE, HUMIDITY, PRESSURE, RAINFALL]
+    data = json.loads(urlopen(LIVE_E_NAIST_URL).read().decode('utf8'))
 
-#twitter = Twython(API_KEY, API_SECRET, ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET)
-#twitter.update_status(status=tweet)
-print(tweet)
+    tweet = ','.join(get_live_e_data_str(data, output_data))
 
+    time = dateutil.parser.parse(data[TEMPERATURE[0]]['time'])
+    tweet += ' 【' + time.strftime("%Y-%m-%d %H:%M:%S") + '】'
+
+    return tweet
+ 
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='Yet Another bot for weathercast at NAIST')
+    parser.add_argument('--debug', '-d', action='store_true', help='debug flag')
+
+    args = parser.parse_args()
+
+    tweet = get_tweet()
+
+    if args.debug:
+        print(tweet)
+    else:
+        twitter = Twython(API_KEY, API_SECRET, ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET)
+        twitter.update_status(status=tweet)
